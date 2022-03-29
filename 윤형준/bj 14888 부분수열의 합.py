a0f00416eb8e6b3569e60d11747ca14086ebdb6d
hanggeee <yoonhangg@gmail.com>
2022-03-29 22:57:45 +0900

    0329 study

---
+++ 윤형준/bj 14888 부분수열의 합.py
@@ -0,0 +1,12 @@
+
+
+from collections import deque
+N = int(input())
+num = map(int, input().split())
+op = list(map(int, input().split()))
+op_list = ['+','-','*','/']
+op_num = []
+for i in range(4):
+
+
+print(op_list)
